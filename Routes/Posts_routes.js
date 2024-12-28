3a03227fb9e0fd158b8307945ce8bd6e6fad5b99
MaxHaGever <maxspectorr@gmail.com>
2024-12-28 18:31:52 +0200

    Lesson 2

--- Routes/Posts_routes.js
+++ Routes/Posts_routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const Post = require("../controllers/post");
 
 router.get("/", Post.getAllPosts);
-
+router.get("/:id", Post.getPostbyId)
 router.post("/", Post.createPost);
+router.delete("/:id",Post.deletePost);
 
 module.exports = router;
--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,49 @@
-const getAllPosts = (req,res)=>{
-    console.log("Posts get All service");
-    res.send("Posts get All service");
+const { json } = require("body-parser");
+const postModel = require("../models/post_model");
+
+const getAllPosts = async (req,res)=>{
+    const ownerFilter = req.query.owner;
+    try{
+        if(ownerFilter){
+        const posts = await postModel.find({owner: ownerFilter});
+        res.status(200).send(posts);
+        } else {
+            const posts = await postModel.find();
+            res.status(200).send(posts);
+        }
+    }   catch(error){
+        res.status(400).send(er);
+    }
 };
 
-const createPost = (req,res)=>{
-    console.log("Posts create post");
-    res.send("Posts create post");
+const getPostbyId = async (req,res) => {
+    const postID = req.params.id;
+    try{
+        const post = await postModel.findById(postID);
+        res.status(200).send(post);
+    }catch (error){
+        res.status(400).send(error);
+    }
+}
+
+const createPost = async (req,res)=>{
+    const post = req.body;
+    try{
+        const newPost = await postModel.create(post);
+        res.status(201).send(newPost);
+    }catch(error){
+        res.status(400).send(error);
+    }
 };
 
-module.exports = {getAllPosts,createPost};
+const deletePost = async (req,res) => {
+    const postID = req.params.id
+    try{
+        const post = await postModel.deleteOne({_id: postID});
+        res.status(200).send(post);
+    }catch(error){
+        res.status(400).send(error);
+    }
+};
+
+module.exports = {getAllPosts,createPost,deletePost,getPostbyId};
