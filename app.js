d71c6599c9650fb06e34c263de44cdd6abbbf523
MaxHaGever <maxspectorr@gmail.com>
2025-01-05 11:13:12 +0200

    DB added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const router = express.Router();
 const dotenv = require('dotenv').config()
 const port = process.env.PORT
+const dbURI = process.env.dbURI
+
+mongoose.connect(dbURI, {})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error('DB connection error:', err));
 
 const indexRouter = require('./routes')
 app.use('/',indexRouter);
